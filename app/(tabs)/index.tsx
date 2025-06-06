9a603714a29797ecca2d96a5291be1d7dd731443
GaneshGoku <sonwaneganesh2005@gmail.com>
2025-06-06 20:11:21 +0530

    commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,8 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1  justify-center items-center" >
-      <Text className="text-center font-bold text-5xl color-primary">welcome</Text>
-      <Link href="/onboarding">Onboarding</Link> 
-      <Link href="/movie/avegers"> Avengers Movie</Link> 
+    <View className="flex-1 bg-primary">
     </View>
   );
 }
